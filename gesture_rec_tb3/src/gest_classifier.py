5052dd7abf8e89e269b157bef16488fa3675f9eb
JadeNotJaded <jadeng66@gmail.com>
2019-11-25 20:58:43 -0500

    commiting

---
+++ gesture_rec_tb3/src/gest_classifier.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import cv2, time
+# Import of keras model and hidden layers for our convolutional network
+# from keras.models import Sequential
+# from keras.layers.convolutional import Conv2D, MaxPooling2D
+# from keras.layers import Dense, Flatten
+from keras.models import load_model
+import numpy as np
+import os
+from PIL import Image
+import glob
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import String
+
+#global variables
+X_test = []  #used to store images
+model = load_model("handrecognition_model.h5") #load hand_recognition model
+
+#get image information from image_ir
+def callback(msg):
+    value = msg
+    depthMap = numby.array([value]) #image that you have subscribed to
+    img = cv2.imread(depthMap) # Reads image and returns np.array
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Converts into the corret colorspace(GRAY)
+    img = cv2.resize(img, (320, 120)) # Reduce image size so training can be faster
+    X_test.append(img)
+    X_test = np.array(X_test, dtype="uint8")
+    X_test = X_test.reshape(len(X_test), 120, 320, 1)
+
+#get prediction
+def getPrediction(model, gray):
+    predictions_array = model.predict(gray) #check this out 
+    for i in range(0,1):
+        return predict(predictions_array[i])
+    
+def predict(prediction):
+    class_names = ["down", "palm", "l", "fist", "fist_moved", "thumb", "index", "ok", "palm_moved", "c"] 
+    predicted_label = np.argmax(prediction)
+    return (class_names[predicted_label])
+
+if __name__ == "__main__":
+    #init node
+    rospy.init_node('gest_classifier')
+    #subscribe to image_ir
+    sub = rospy.Subscriber('/image_ir', String, callback) #get full path and type
+    #get_prediciton
+    prediction = getPrediction(model, X_test) #check this out
+    #publish predicition
+    pub = rospy.Publisher('/command', String, queue_size=10) #publish command 
+    pub.publish(prediction)
+    rospy.spin()
---
+++ gesture_rec_tb3/src/instruction.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import String
+
+#global variables
+cmd = ''
+distance_ahead = 0
+vel_msg = Twist()
+
+def getDistanceAhead(msg):
+    distance_ahead = msg.ranges[len(msg.ranges)/2]
+
+def getCommand(msg):
+    cmd = msg
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0
+    if cmd == 'palm': #stop
+           vel_msg.linear.y = 0
+    elif cmd == 'thumb': #move forward
+        vel_msg.linear.x = 0.1
+    elif cmd == 'l': #turn 1 degree left
+        vel_msg.angular.z = -0.1
+        vel_msg.linear.x = 0.1
+    elif cmd == 'c': #turn 1 degree right
+        vel_msg.angular.z = 0.1
+        vel_msg.linear.x = 0.10
+
+def isSafe(command):
+    if(distance_ahead < 10):
+        return False
+    return True
+
+if __name__ == "__main__":
+    #init node
+    rospy.init_node('instruction')
+    rospy.Subscriber('/command', String, getCommand)
+    scan_sub = rospy.Subscriber('scan', LaserScan, getDistanceAhead)
+    twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1) 
+    if(isSafe(cmd)):
+        twist_pub.publish(vel.msg)
+    else:
+        twist_pub
